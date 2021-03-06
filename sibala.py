7ed0e62727c9742b6f371659d17e348000852082
james <yehjames23@gmail.com>
2021-03-06 16:19:55 +0800

    elit & eugene & james

--- sibala.py
+++ sibala.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import IntEnum
 import collections
 
@@ -15,16 +16,27 @@ category_dict = {
 }
 
 
+@dataclass
+class Output(object):
+    score: int
+    max_valid_dice: int
+
+    def __gt__(self, other):
+        return (self.score, self.max_valid_dice) > (other.score, other.max_valid_dice)
+
+
 class Player(object):
     name: str
     dices: list[int]
+    score: int
+    max_dice: int
     category_type: DiceCategory
-    output: tuple[int, int]
+    output: Output
 
     def __init__(self, name: str, dices: list[int]):
         self.name = name
         self.dices = dices
-        self.category_type = self._get_category(self.dices)
+        self.category_type = self._get_category()
         self.output = self._get_output()
 
     def __repr__(self):
@@ -33,9 +45,9 @@ class Player(object):
     def _get_output(self):
         c = collections.Counter(self.dices)
         if (n := len(c.keys())) == 1:
-            return self.dices[0], 0
+            return Output(self.dices[0], 0)
         elif n == 4:
-            return 0, 0
+            return Output(0, 0)
         elif n == 3:
             # 6, 6, 1, 2
             res_s, res_m = 0, 0
@@ -44,16 +56,15 @@ class Player(object):
                     continue
                 res_s += k
                 res_m = max(res_m, k)
-            return res_s, res_m
+            return Output(res_s, res_m)
         else:
             if set(c.values()) == set([1, 3]):
-                return 0, 0
+                return Output(0, 0)
             # 6, 6, 2, 2
-            return max(c.keys()) * 2, 0
+            return Output(max(c.keys()) * 2, 0)
 
-
-    def _get_category(self, dices: list[int]) -> DiceCategory:
-        c = collections.Counter(dices)
+    def _get_category(self) -> DiceCategory:
+        c = collections.Counter(self.dices)
         if (n := len(c.keys())) == 1:
             return DiceCategory.ALL_THE_SAME_KIND
         elif n == 4:
@@ -63,24 +74,22 @@ class Player(object):
                 return DiceCategory.NO_POINTS
         return DiceCategory.NORMAL_POINT
 
-    def __cmp__(self, other):
-        pass
-
 
 def get_winner(p1: Player, p2: Player) -> tuple[bool, Player, int]:
     if p1.category_type != p2.category_type:
         winner = p1 if p1.category_type > p2.category_type else p2
-        return True, winner, winner.output[0]
+        return True, winner, winner.output.score
     if p1.category_type == DiceCategory.ALL_THE_SAME_KIND:
-        winner = p1 if p1.category_type > p2.category_type else p2
-        return True, winner, winner.output[0]
+        if p1.output != p2.output:
+            winner = p1 if p1.output > p2.output else p2
+            return True, winner, winner.output.score
+        return False, None, None
     elif p1.category_type == DiceCategory.NORMAL_POINT:
         if p1.output != p2.output:
             winner = p1 if p1.output > p2.output else p2
-            return True, winner, winner.output[0] if p1.output[0] != p2.output[0] else winner.output[1]
+            return True, winner, winner.output.score if p1.output.score != p2.output.score else winner.output.max_valid_dice
 
     return False, None, None
-
 
 
 class Sibala(object):
@@ -106,8 +115,3 @@ class Sibala(object):
             return f"{winner.name} wins. {category_dict[winner.category_type]}: {output}"
 
         return "Tie."
-
-
-
-
-
